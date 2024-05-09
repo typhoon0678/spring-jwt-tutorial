70dd83363fb210c20f0c599ec378c0292c81895a
typhoon0678 <typhoon0678@gmail.com>
2024-05-09 18:07:04 +0900

    feat: initial setting

---
+++ src/test/java/com/typhoon0678/jwttutorial/JwtTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.typhoon0678.jwttutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
